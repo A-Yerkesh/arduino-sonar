64815ca8c47b1c4391ed04f34824bd932801c7b7
Arman Yerkesh <a.yerkesh@gmail.com>
2026-06-22 08:12:47 +0300

    Fallback to Serial if OLED is unavailable

--- app/app_display.cpp
+++ app/app_display.cpp
@@ -1,14 +1,26 @@
 #include "app_display.h"
 #include "../drv/drv_oled.h"
+#include "../drv/drv_serial.h"
+
+static bool s_use_serial = false;
 
 sonar_status_t app_display_init(void) {
-    return drv_oled_init();
+    sonar_status_t status = SONAR_ERR_GENERIC;
+    if (drv_oled_init() == SONAR_OK) {
+        status = SONAR_OK;
+    } else if (drv_serial_init() == SONAR_OK) {
+        s_use_serial = true;
+        status = SONAR_OK;
+    }
+    return status;
 }
 
 void app_display_update(drv_ultrasonic_result_t result) {
-    if (result.status == SONAR_OK) {
-        drv_oled_show_distance(result.dist_cm);
+    if (s_use_serial) {
+        if (result.status == SONAR_OK) drv_serial_show_distance(result.dist_cm);
+        else                           drv_serial_show_error(result.status);
     } else {
-        drv_oled_show_error(result.status);
+        if (result.status == SONAR_OK) drv_oled_show_distance(result.dist_cm);
+        else                           drv_oled_show_error(result.status);
     }
 }
